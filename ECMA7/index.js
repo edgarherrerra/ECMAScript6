960a6d77a99857f5aab6bb73eaac094e7e73736f
Edgar Herera <contactoedgarherrera@gmail.com>
2020-02-02 14:58:38 -0600

    Includes and Pows.

---
+++ ECMA7/index.js
@@ -0,0 +1,19 @@
+/**
+ * Includes
+ */
+let numbers = [1, 2, 3, 7, 8];
+constVALUE = 7;
+
+if (numbers.includes(VALUE)) {
+  console.log(`Value: ${VALUE}`);
+} else {
+  console.log(`Value: ${VALUE}`);
+}
+
+/**
+ * Pow
+ */
+let base = 4;
+let exponent = 4;
+letresult = base ** exponent;
+console.log(`Result -> ${result}`);
